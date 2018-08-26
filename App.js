988ffae4148b4503b0760095b58ae2231db38ab1
aiibe <hakmamy@hotmail.com>
2018-08-27 00:54:37 +0700

    remove unnecessary code

--- App.js
+++ App.js
@@ -1,51 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import Logo from './src/components/svg/logo';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      styleContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      }
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  // Method accessible
+  // via this.changeBackground()
+  changeBackground(){
+    const newStyleContainer = {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'blue',
+      }
+    this.setState({ styleContainer: newStyleContainer })
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Logo />
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={this.state.styleContainer}>
+        <Logo style={{ marginBottom:20 }} />
+        <Button
+          onPress={() => this.changeBackground() } 
+          title="Tap to change background color" />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App
